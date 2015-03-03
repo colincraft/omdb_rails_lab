bf039d58460a375297173748896e9ec7ad748334
Tim Licata <tim@timothylicata.com>
2015-03-02 19:17:38 -0800

    index method searches OMDB and renders results into template

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,16 @@
 class MoviesController < ApplicationController
   def index
+    @search_results = []
+
+    if params.key? :search_term
+      request = Typhoeus::Request.new(
+        "http://www.omdbapi.com/",
+        method: :get,
+        params: {s: params[:search_term]}
+      )
+      request.run
+      parsed = JSON.parse(request.response.body)
+      @search_results = parsed["Search"]
+    end
   end
 end
